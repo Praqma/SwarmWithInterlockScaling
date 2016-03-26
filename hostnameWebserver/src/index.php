e13b840adc77196ce48ebbf67d125a7fe5f2b1b8
hoeghh <heh@praqma.net>
2016-03-26 20:35:11 +0100

    better php ip page

--- hostnameWebserver/src/index.php
+++ hostnameWebserver/src/index.php
@@ -1,5 +1,7 @@
 <?php
-  echo "Hostname  : " . gethostname();
+  echo "Hostname     : " . gethostname();
   echo "<br>";
-  echo "Remote IP : " . $_SERVER['REMOTE_ADDR'];
+  echo "PHP Server IP: " . $_SERVER['SERVER_ADDR'];
+  echo "<br>";
+  echo "Container IP : " . shell_exec("cat /etc/hosts |grep -m 1 web.docker.demo");
 ?>
